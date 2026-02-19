93ebe5f29c4677cf8309f49550b07deff562f74d
Ankit Ydv <ankityadav11241@gmail.com>
2026-02-19 23:29:30 +0530

    crud

--- src/main/java/com/ankit/Main.java
+++ src/main/java/com/ankit/Main.java
@@ -12,18 +12,23 @@ public class Main {
         s1.setRollNo(3);
         s1.setAge(21);
 
+        Student s2 = null;
+
         Configuration cfg = new Configuration();
         cfg.addAnnotatedClass(com.ankit.Student.class);
         cfg.configure();
 
         SessionFactory sf = cfg.buildSessionFactory();
         Session session = sf.openSession();
-        Transaction tx = session.beginTransaction();
-        session.persist(s1);
-        tx.commit();
+//        Transaction tx = session.beginTransaction();
+//        session.persist(s1);
+//        tx.commit();
+        s2 = session.detach(Student.class, 1);
+        System.out.println("got results:" + s2);
+
         session.close();
         sf.close();
 
-        System.out.println(s1);
+//        System.out.println(s1);
     }
 }
